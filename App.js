8828eba89f687ce53bb8fd053d070e7f124cd209
snv <snv@csod.com>
2023-09-22 05:47:29 +0530

    01 Commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h2", { id: "heading" }, " HEllo World!");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
